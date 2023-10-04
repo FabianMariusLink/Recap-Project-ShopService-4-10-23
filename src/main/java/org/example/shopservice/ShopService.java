c8857b93feaa128ff28c875805d8249310178024
FabianMariusLink <fabian.marius.link@gmail.com>
2023-10-04 15:12:14 +0200

    In class ShopService refactor method placeOrder

--- src/main/java/org/example/shopservice/ShopService.java
+++ src/main/java/org/example/shopservice/ShopService.java
@@ -5,12 +5,18 @@ import java.util.UUID;
 
 public class ShopService {
     public void placeOrder(String customerName, ArrayList<Product> orderProducts, OrderListRepo orderListRepo, ProductRepro productRepo) {
+        //hier fehlt noch die komplette Prüfung der vorhandenen Produkte
+        boolean productsInStock = true;
+
         for(int i = 0; i <= orderProducts.size()-1; i++) {
-            if(productRepo.searchProductByName(orderProducts.get(i).name()) != null){
+            if (productRepo.searchProductByName(orderProducts.get(i).name()) == null) {
+                productsInStock = false;
+            }
+        }
+            if(productsInStock){
                 orderListRepo.addOrder(new Order(UUID.randomUUID().toString(), customerName, orderProducts));
             }else{
                 System.out.println("Product(s) not in stock");
             }
-        }
     }
 }
