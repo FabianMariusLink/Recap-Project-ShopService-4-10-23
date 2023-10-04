5b019563727ebf5729fc3e5cf9a2b4ff99d7e294
FabianMariusLink <fabian.marius.link@gmail.com>
2023-10-04 11:46:13 +0200

    Add class OrderListRepo

---
+++ src/main/java/org/example/shopservice/OrderListRepo.java
@@ -0,0 +1,50 @@
+package org.example.shopservice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class OrderListRepo {
+
+    List<Order> orders;
+
+    public OrderListRepo() {
+        orders = new ArrayList<>();
+    }
+
+    public void addOrder(Order order){
+        orders.add(order);
+    }
+
+    public void deleteOrder(Order order){
+        orders.remove(order);
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderListRepo that = (OrderListRepo) o;
+        return Objects.equals(orders, that.orders);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orders);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderListRepo{" +
+                "orders=" + orders +
+                '}';
+    }
+}
